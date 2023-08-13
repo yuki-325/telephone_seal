01c909c0edc4043c0a065605159890b664b4a42a
中野勇貴 <sakeikura325325@gmail.com>
2023-08-13 21:37:19 +0900

    [fix] appのテーマ関連を作成

--- lib/common/theme/app_theme.dart
+++ lib/common/theme/app_theme.dart
@@ -2,39 +2,52 @@ import 'package:flutter/material.dart';
 
 /// アプリのテーマを管理するクラスです。
 class AppTheme {
-  // 共通のテーマ設定を定義します
+// テーマのベースとなるテーマデータを定義します
   static final ThemeData baseTheme = ThemeData(
-    textSelectionTheme:
-        const TextSelectionThemeData(cursorColor: Colors.orange),
-    // fontFamily: 'SourceSansPro',
+    // テキスト選択のテーマデータを設定します
+    textSelectionTheme: const TextSelectionThemeData(cursorColor: Colors.white),
+    // テキストのテーマデータを設定します
     textTheme: TextTheme(
+      // 小さな表示テキストのスタイルを設定します
       displaySmall: const TextStyle(
         fontFamily: 'OpenSans',
         fontSize: 45.0,
-        // fontWeight: FontWeight.w400,
-        color: Colors.orange,
+        color: Colors.white,
       ),
+      // 大文字ラベルのスタイルを設定します
       labelLarge: const TextStyle(
-        // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
         fontFamily: 'OpenSans',
       ),
+      // 小さな本文テキストのスタイルを設定します
       bodySmall: TextStyle(
         fontFamily: 'NotoSans',
         fontSize: 12.0,
         fontWeight: FontWeight.normal,
         color: Colors.deepPurple[300],
       ),
+      // 大きな表示テキストのスタイルを設定します
       displayLarge: const TextStyle(fontFamily: 'Quicksand'),
+      // 中くらいの表示テキストのスタイルを設定します
       displayMedium: const TextStyle(fontFamily: 'Quicksand'),
+      // 中見出しのスタイルを設定します
       headlineMedium: const TextStyle(fontFamily: 'Quicksand'),
+      // 小見出しのスタイルを設定します
       headlineSmall: const TextStyle(fontFamily: 'NotoSans'),
+      // 大きなタイトルのスタイルを設定します
       titleLarge: const TextStyle(fontFamily: 'NotoSans'),
+      // 中くらいのタイトルのスタイルを設定します
       titleMedium: const TextStyle(fontFamily: 'NotoSans'),
+      // 大きな本文のスタイルを設定します
       bodyLarge: const TextStyle(fontFamily: 'NotoSans'),
+      // 中くらいの本文のスタイルを設定します
       bodyMedium: const TextStyle(fontFamily: 'NotoSans'),
+      // 小さなタイトルのスタイルを設定します
       titleSmall: const TextStyle(fontFamily: 'NotoSans'),
+      // 小さなラベルのスタイルを設定します
       labelSmall: const TextStyle(fontFamily: 'NotoSans'),
     ),
+
+    // カラースキームを設定します
     colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
         .copyWith(secondary: Colors.orange),
   );
