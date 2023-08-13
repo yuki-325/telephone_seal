00a5ab18c27fe1523e1cb764e5c29a5910ed54b5
中野勇貴 <sakeikura325325@gmail.com>
2023-08-13 21:36:08 +0900

    [fix] 名前変更とファイル移動

---
+++ lib/common/constants/app_labels.dart
@@ -0,0 +1,26 @@
+class AppLabels {
+  static const String appName = 'Telephone Seal';
+  // static const String logoTag = 'near.huscarl.loginsample.logo';
+  // static const String titleTag = 'near.huscarl.loginsample.title';
+// ログイン関連
+  static const String loginButtonLabel = "ログイン";
+  static const String signUpButtonLabel = "新規登録";
+  static const String forgotPasswordLabel = "パスワードをお忘れですか？";
+  static const String createAccountLabel = "アカウントを作成";
+
+  // サインアップ関連
+  static const String fullNameLabel = "フルネーム";
+  static const String emailLabel = "メールアドレス";
+  static const String passwordLabel = "パスワード";
+  static const String confirmPasswordLabel = "パスワード（確認）";
+
+  // パスワードリカバリ関連
+  static const String recoverPasswordLabel = "パスワードをリカバリして下さい";
+  static const String enterEmailLabel = "登録したメールアドレスを入力してください";
+  static const String sendRecoveryEmailLabel = "リカバリメールを送信";
+
+  // 共通
+  static const String backButtonLabel = "戻る";
+  static const String continueButtonLabel = "次へ";
+  static const String submitButtonLabel = "送信";
+}
